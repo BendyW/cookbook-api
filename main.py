46f7dc11653a4408ad11589ad6fff936294df680
Ben Waldman <benmw2@gmail.com>
2024-02-12 14:02:15 -0600

    tweaking chatgpt still

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ from bs4 import BeautifulSoup
 from chatgptmax import send
 
 app = FastAPI()
-print("test")
+print("begin test")
 
 config = dotenv_values()
 
@@ -23,19 +23,12 @@ headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleW
 r = requests.get(testingURL, headers=headers)
 soup = BeautifulSoup(r.content.decode(), 'html.parser')
 soup = soup.get_text().strip()
-
-responses = send(prompt="Return the recipe from this text:", text_data=soup, chat_model="gpt-3.5-turbo", model_token_limit=modelTokenLimit, max_tokens=sendTokenLimit)
+# "Return the recipe from this text only where a recipe is explicitly stated:"
+findIngredientsAndInstructionsResponses = send(prompt="Check if this is an exact list of ingredients with measurements. Return the ingredients and measurements if it is a match. Check if this is exact instructions to a recipe. Return the instructions if it is a match. Do not return text if no matches", text_data=soup, chat_model="gpt-3.5-turbo", model_token_limit=modelTokenLimit, max_tokens=sendTokenLimit)
 allResponses = ""
-for response in responses:
+for response in findIngredientsAndInstructionsResponses:
     allResponses = allResponses + response.content
-
 print(allResponses)
-print("--------------------")
-print("Removing unnecessary text")
-print("--------------------")
-cleanResponses = send(prompt="Return most correct instructions:", text_data=allResponses, chat_model="gpt-3.5-turbo", model_token_limit=modelTokenLimit, max_tokens=sendTokenLimit)
-for cleanResponses in cleanResponses:
-    print(cleanResponses.content)
 print("End of test")
 
 # class Recipe(BaseModel):
